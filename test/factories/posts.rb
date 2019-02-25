24a7ebbdca99b805294069af7961e3055312ee55
Mark Harmsen <mharmsenr@gmail.com>
2019-02-25 15:46:59 -0300

    Search posts: test

--- test/factories/posts.rb
+++ test/factories/posts.rb
@@ -1,15 +1,17 @@
 FactoryBot.define do
+
   factory :post do
     title { Faker::Lorem.sentence }
     content { Faker::Lorem.paragraph }
-    published { 
-      r = rand(0..1)
-      if r == 0
-        false
-      else
-        true
-      end
-    }
+    published { 0 == rand(0..1) }
     user
   end
+
+  factory :published_post, class: 'Post' do
+    title { Faker::Lorem.sentence }
+    content { Faker::Lorem.paragraph }
+    published { true }
+    user
+  end
+
 end
