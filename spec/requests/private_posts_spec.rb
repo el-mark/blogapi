d268168d65a698e111f03e8bd7184e540483ea30
Mark Harmsen <mharmsenr@gmail.com>
2019-02-26 12:03:42 -0300

    Auth: add logic to create, show and update

--- spec/requests/private_posts_spec.rb
+++ spec/requests/private_posts_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "Posts with authentication", type: :request do
                     before { get "/posts/#{other_user_post.id}", headers: auth_headers}
 
                     context "payload" do
-                        subject { JSON.parse(response.body )}
+                        subject { payload }
                         it { is_expected.to include(:id) }
                     end
                     context "response" do
@@ -28,7 +28,7 @@ RSpec.describe "Posts with authentication", type: :request do
                     before { get "/posts/#{other_user_post_draft.id}", headers: auth_headers}
                     
                     context "payload" do
-                        subject { JSON.parse(response.body )}
+                        subject { payload }
                         it { is_expected.to include(:error) }
                     end
                     context "response" do
@@ -48,11 +48,10 @@ RSpec.describe "Posts with authentication", type: :request do
     describe "PUT /posts/{id}" do
     
     end
-    #     it "should return OK" do
-    #         get '/posts'
-    #         payload = JSON.parse(response.body)
-    #         expect(payload).to be_empty
-    #         expect(response).to have_http_status(200)
-    #     end
-    # end
+    
+    private
+
+    def payload
+        JSON.parse(response.body).with_indifferent_access
+    end
 end
